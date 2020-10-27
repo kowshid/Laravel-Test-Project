fcd601bbb5d2e0172deced1b5da00cf0461a456d
kowshid <kowshid@gmail.com>
2020-10-27 14:29:38 +0600

    Blade Template Use

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -1,15 +1,7 @@
+@extends('layouts.app')
 
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-
-        <title>{{config('app.name', 'LTEST')}}</title>
-    </head>
-    <body>
-        <h1>About</h1>
-        <p>This is the about page</p>
-        <p>Test Laravel Project by Kowshid</p>
-    </body>
-</html>
+@section('content')
+    <h1>About</h1>
+    <p>This is the about page</p>
+    <p>Test Laravel Project by Kowshid</p>
+@endsection
--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -1,13 +1,6 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+@extends('layouts.app')
 
-        <title>{{config('app.name', 'LTEST')}}</title>
-    </head>
-    <body>
-        <h1>Laravel Test Project</h1>
-        <p>This my laravel project from scracth</p>
-    </body>
-</html>
+@section('content')
+    <h1>Laravel Test Project</h1>
+    <p>This my laravel project from scracth</p>
+@endsection
