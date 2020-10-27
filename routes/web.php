e79b403d3d8afeb02916a06615a341f93465c002
kowshid <kowshid@gmail.com>
2020-10-27 13:31:25 +0600

    about page routing

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/hello', function () {
+    return 'Hello World';
+});
+
+Route::get('/about', function() {
+    return view('pages.about');
+});
